dfbdb0896f85cb39f76d9d136e7567f476fd4fbf
cosmineala <39276437+cosmineala@users.noreply.github.com>
2020-05-21 05:10:47 -0700

    0.8.1
    + Admin pages

---
+++ ReadDeal/RealDeal/Controllers/AdminAuctionRegistrationsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RealDeal.AppLogic.Models;
+using RealDeal.DataAccess;
+
+namespace RealDeal.Controllers
+{
+    public class AdminAuctionRegistrationsController : Controller
+    {
+        private readonly DataAccessDbContext _context;
+
+        public AdminAuctionRegistrationsController(DataAccessDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AdminAuctionRegistrations
+        public async Task<IActionResult> Index()
+        {
+            var dataAccessDbContext = _context.AuctionRegistrations.Include(a => a.Item).Include(a => a.User);
+            return View(await dataAccessDbContext.ToListAsync());
+        }
+
+        // GET: AdminAuctionRegistrations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var auctionRegistration = await _context.AuctionRegistrations
+                .Include(a => a.Item)
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(m => m.UserID == id);
+            if (auctionRegistration == null)
+            {
+                return NotFound();
+            }
+
+            return View(auctionRegistration);
+        }
+
+        // GET: AdminAuctionRegistrations/Create
+        public IActionResult Create()
+        {
+            ViewData["ItemID"] = new SelectList(_context.Items, "ID", "ID");
+            ViewData["UserID"] = new SelectList(_context.Users, "ID", "ID");
+            return View();
+        }
+
+        // POST: AdminAuctionRegistrations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserID,ItemID")] AuctionRegistration auctionRegistration)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(auctionRegistration);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ItemID"] = new SelectList(_context.Items, "ID", "ID", auctionRegistration.ItemID);
+            ViewData["UserID"] = new SelectList(_context.Users, "ID", "ID", auctionRegistration.UserID);
+            return View(auctionRegistration);
+        }
+
+        // GET: AdminAuctionRegistrations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var auctionRegistration = await _context.AuctionRegistrations.FindAsync(id);
+            if (auctionRegistration == null)
+            {
+                return NotFound();
+            }
+            ViewData["ItemID"] = new SelectList(_context.Items, "ID", "ID", auctionRegistration.ItemID);
+            ViewData["UserID"] = new SelectList(_context.Users, "ID", "ID", auctionRegistration.UserID);
+            return View(auctionRegistration);
+        }
+
+        // POST: AdminAuctionRegistrations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserID,ItemID")] AuctionRegistration auctionRegistration)
+        {
+            if (id != auctionRegistration.UserID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(auctionRegistration);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AuctionRegistrationExists(auctionRegistration.UserID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ItemID"] = new SelectList(_context.Items, "ID", "ID", auctionRegistration.ItemID);
+            ViewData["UserID"] = new SelectList(_context.Users, "ID", "ID", auctionRegistration.UserID);
+            return View(auctionRegistration);
+        }
+
+        // GET: AdminAuctionRegistrations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var auctionRegistration = await _context.AuctionRegistrations
+                .Include(a => a.Item)
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(m => m.UserID == id);
+            if (auctionRegistration == null)
+            {
+                return NotFound();
+            }
+
+            return View(auctionRegistration);
+        }
+
+        // POST: AdminAuctionRegistrations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var auctionRegistration = await _context.AuctionRegistrations.FindAsync(id);
+            _context.AuctionRegistrations.Remove(auctionRegistration);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AuctionRegistrationExists(int id)
+        {
+            return _context.AuctionRegistrations.Any(e => e.UserID == id);
+        }
+    }
+}
---
+++ ReadDeal/RealDeal/Controllers/AdminItemsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RealDeal.AppLogic.Models;
+using RealDeal.DataAccess;
+
+namespace RealDeal.Controllers
+{
+    public class AdminItemsController : Controller
+    {
+        private readonly DataAccessDbContext _context;
+
+        public AdminItemsController(DataAccessDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AdminItems
+        public async Task<IActionResult> Index()
+        {
+            var dataAccessDbContext = _context.Items.Include(i => i.Buyer).Include(i => i.Owner);
+            return View(await dataAccessDbContext.ToListAsync());
+        }
+
+        // GET: AdminItems/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items
+                .Include(i => i.Buyer)
+                .Include(i => i.Owner)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // GET: AdminItems/Create
+        public IActionResult Create()
+        {
+            ViewData["BuyerID"] = new SelectList(_context.Users, "ID", "ID");
+            ViewData["OwnerID"] = new SelectList(_context.Users, "ID", "ID");
+            return View();
+        }
+
+        // POST: AdminItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,StartPrice,AuctionDate,AuctionDeadline,Description,Tag,OwnerID,BuyerID")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BuyerID"] = new SelectList(_context.Users, "ID", "ID", item.BuyerID);
+            ViewData["OwnerID"] = new SelectList(_context.Users, "ID", "ID", item.OwnerID);
+            return View(item);
+        }
+
+        // GET: AdminItems/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            ViewData["BuyerID"] = new SelectList(_context.Users, "ID", "ID", item.BuyerID);
+            ViewData["OwnerID"] = new SelectList(_context.Users, "ID", "ID", item.OwnerID);
+            return View(item);
+        }
+
+        // POST: AdminItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,StartPrice,AuctionDate,AuctionDeadline,Description,Tag,OwnerID,BuyerID")] Item item)
+        {
+            if (id != item.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(item);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ItemExists(item.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BuyerID"] = new SelectList(_context.Users, "ID", "ID", item.BuyerID);
+            ViewData["OwnerID"] = new SelectList(_context.Users, "ID", "ID", item.OwnerID);
+            return View(item);
+        }
+
+        // GET: AdminItems/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items
+                .Include(i => i.Buyer)
+                .Include(i => i.Owner)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // POST: AdminItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            _context.Items.Remove(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ItemExists(int id)
+        {
+            return _context.Items.Any(e => e.ID == id);
+        }
+    }
+}
